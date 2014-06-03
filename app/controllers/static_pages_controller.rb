0e5f9e13894ffce9d2874535190a895b9ef18a24
Wu Zhi Jie <wzj_12@163.com>
2014-06-03 09:57:53 +0800

    更新搜索bug

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,19 +8,20 @@ class StaticPagesController < ApplicationController
     @list = []
     keyword = params[:keyword]
     @zz = "http://zz.xinwowang.com"
+    @xm = "http://xm.xinwowang.com"
     @qz = "http://qz.xinwowang.com"
     @gz = "http://gz.xinwowang.com"
     @sz = "http://sz.xinwowang.com"
     # new_homes
-    [@zz, @qz, @gz, @sz].map do |city|
+    [@zz, @xm, @qz, @gz, @sz].map do |city|
       city_list = JSON.parse(open(URI.escape("#{city}/new_homes.json?keyword=#{keyword}")).read)
-      get_search_list(city_list, "new_homes")
+      get_search_list(city_list, "new_homes", city)
     end
 
     # information
-    [@zz, @qz, @gz, @sz].map do |city|
+    [@zz, @xm, @qz, @gz, @sz].map do |city|
       city_list = JSON.parse(open(URI.escape("#{city}/information.json?keyword=#{keyword}")).read)
-      get_search_list(city_list, "information")
+      get_search_list(city_list, "information", city)
     end
 
     @list = @list.paginate(page: params[:page], per_page: 20)
@@ -32,13 +33,13 @@ class StaticPagesController < ApplicationController
   end
 
   private
-    def get_search_list(url, model_name)
+    def get_search_list(url, model_name, city_lnk)
 
       url.each do |item|
         @list << [
                   item["name"] ? item["name"] : item["title"],
                   item["description"],
-                  "#{@zz}/#{model_name}/#{item["id"]}"
+                  "#{city_lnk}/#{model_name}/#{item["id"]}"
                  ]
       end
     end
