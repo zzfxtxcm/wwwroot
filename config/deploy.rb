de87cef39002664ea30522327f08cf0b252daa4b
Wu Zhi Jie <wzj_12@163.com>
2014-06-20 19:48:01 +0800

    tmp

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :linked_files, %w{config/database.yml config.ru}
 #
 # dirs we want symlinking to shared
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets}
+set :linked_dirs, %w{bin log public/uploads tmp/pids tmp/cache tmp/sockets}
 
 set :keep_releases, 5
 
